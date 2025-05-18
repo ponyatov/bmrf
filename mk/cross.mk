6ae0299930da70193e664d496966aefac0b4deb2
Dmitry Ponyatov <dponyatov@gmail.com>
2025-05-18 23:10:11 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/cross.mk
+++ mk/cross.mk
@@ -17,3 +17,8 @@ elf: $(ELF)
 dfu: $(DFU)
 $(DFU): $(ELF)
 	~/elf2dfuse/bin/elf2dfuse $< $@
+
+.PHONY: i486
+i486:
+	rustup component add rust-src --toolchain nightly-x86_64-unknown-linux-gnu
+	cargo +nightly build -Zbuild-std --target .cargo/i486-pc-bare.json
