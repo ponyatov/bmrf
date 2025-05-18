d6ee241699d5927fd7598578630f81dac0ac4448
Dmitry Ponyatov <dponyatov@gmail.com>
2025-05-18 22:15:48 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/bare.rs
+++ src/bare.rs
@@ -1,1 +1,31 @@
 // https://stackoverflow.com/questions/67902309/how-to-compile-rust-code-to-bare-metal-32-bit-x86-i686-code-what-compile-targ
+
+#![cfg(feature = "bare")]
+#![no_std]
+// disables Rust runtime init,
+#![no_main]
+// see https://docs.rust-embedded.org/embedonomicon/smallest-no-std.html
+#![feature(lang_items)]
+
+// see https://docs.rust-embedded.org/embedonomicon/smallest-no-std.html
+#[lang = "eh_personality"]
+extern "C" fn eh_personality() {}
+
+use core::panic::PanicInfo;
+use core::sync::atomic;
+use core::sync::atomic::Ordering;
+
+#[no_mangle]
+/// The name **must be** `_start`, otherwise the compiler throws away all code as unused.
+/// The name can be changed by passing a different entry symbol as linker argument.
+fn _start() -> ! {
+    loop {}
+}
+
+#[inline(never)]
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {
+        atomic::compiler_fence(Ordering::SeqCst);
+    }
+}
