ae6974a1e2f92dc73d704a1723bbd096e8794fb9
Dmitry Ponyatov <dponyatov@gmail.com>
2025-05-18 15:22:28 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    // \ args
+    let argv: Vec<String> = std::env::args().collect();
+    let argc = argv.len();
+    let ini = if argc > 1 { &argv[1] } else { "lib/ini.ini" };
+    let src = std::fs::read_to_string(ini).unwrap();
+    println!("#{:?} {:?} -> {:?}", argc, argv, ini);
+    // / args
+    println!("\n{:?}\n", src);
 }
