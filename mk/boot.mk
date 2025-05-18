a6d270e51506858c4e45f55a0630472fab10462b
Dmitry Ponyatov <dponyatov@gmail.com>
2025-05-18 21:23:19 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/boot.mk
+++ mk/boot.mk
@@ -1,8 +1,14 @@
 ISOLINUX += root/isolinux/isohdpfx.bin
 ISOLINUX += root/isolinux/isohdppx.bin
 ISOLINUX += root/isolinux/isolinux.bin
+
+ISOLINUX += root/EFI/BOOT/ldlinux.e64
+ISOLINUX += root/EFI/BOOT/syslinux.c32
+
 .PHONY: isolinux
 isolinux: $(ISOLINUX)
 
 root/isolinux/%: /usr/lib/ISOLINUX/%
+	cp $< $@
+root/EFI/BOOT/%: /usr/lib/syslinux/modules/efi64/%
 	cp $< $@
