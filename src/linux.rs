f14083138a0d53ff4961001cfd2cabe2effdc7bb
Dmitry Ponyatov <dponyatov@gmail.com>
2025-05-18 15:56:54 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ src/linux.rs
@@ -0,0 +1,15 @@
+// #![allow(unused_variables)]
+// #![allow(dead_code)]
+// #![allow(unused_imports)]
+
+#[cfg(target_os = "linux")]
+fn main() {
+    // \ args
+    let argv: Vec<String> = std::env::args().collect();
+    let argc = argv.len();
+    let ini = if argc > 1 { &argv[1] } else { "lib/ini.ini" };
+    let src = std::fs::read_to_string(ini).unwrap();
+    println!("#{:?} {:?} -> {:?}", argc, argv, ini);
+    // / args
+    println!("\n{:?}\n", src);
+}
