c641091715bea544671a5d2b15b9c2f19f1c3cdf
Dmitry Ponyatov <dponyatov@gmail.com>
2025-05-18 16:31:34 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/bmrf.rs
+++ src/bmrf.rs
@@ -1,4 +1,7 @@
+#![allow(unused_imports)]
+#![cfg(feature = "cortexM")]
 #![no_std]
+#![cfg(feature = "cortexM")]
 #![no_main]
 
 #[cfg(feature = "linux")]
@@ -6,25 +9,29 @@ pub mod linux;
 #[cfg(feature = "linux")]
 use crate::linux::args;
 
-#[cfg(feature = "cortex")]
-pub mod cortex;
-#[cfg(feature = "cortex")]
-use crate::cortex::args;
-
 #[cfg(feature = "linux")]
 fn main() {
     args();
 }
 
-#[cfg(feature = "cortex")]
+#[cfg(feature = "cortexM")]
+pub mod cortex;
+#[cfg(feature = "cortexM")]
+use crate::cortex::args;
+
+#[cfg(feature = "cortexM")]
 use cortex_m::asm;
-#[cfg(feature = "cortex")]
+#[cfg(feature = "cortexM")]
 use cortex_m_rt::entry;
 
-#[cfg(feature = "cortex")]
+#[cfg(feature = "cortexM")]
+use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+
+#[cfg(feature = "cortexM")]
+#[allow(dead_code)]
 fn main() -> ! {
     asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
-
+    args();
     loop {
         // your code goes here
     }
--- src/cortex.rs
+++ src/cortex.rs
@@ -4,3 +4,5 @@
 // use panic_abort as _; // requires nightly
 // use panic_itm as _; // logs messages over ITM; requires ITM support
 // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+
+pub fn args() {}
