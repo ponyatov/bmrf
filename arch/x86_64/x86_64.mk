a42ec312c3f96d460c04578b55ac2812aad9f7ea
Dmitry Ponyatov <dponyatov@gmail.com>
2025-05-18 15:16:01 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- arch/x86_64/x86_64.mk
+++ arch/x86_64/x86_64.mk
@@ -1,2 +1,4 @@
-OS  ?= linux
-QEMU = qemu-system-$(ARCH)
+OS     ?= linux
+ TARGET = $(ARCH)-unknown-$(OS)-gnu
+RTARGET = $(ARCH)-unknown-$(OS)-gnu
+QEMU    = qemu-system-$(ARCH)
--- os/linux/linux.mk
+++
@@ -1,2 +0,0 @@
-TARGET = $(ARCH)-unknown-linux-gnu
-RTARGET = x86_64-unknown-linux-gnu
